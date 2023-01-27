8c69409ab7c6018754642aaaeacf69c3e5f1fd78
KunalDhruw <108332747+KunalDhruw@users.noreply.github.com>
2023-01-27 20:09:57 +0530

    Update Index.js

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
-export const Index = () =>{
-    return (
-     
-    )
-}
+export const Index = () => {
+  return <div id="index-page">Index Page</div>;
+};
