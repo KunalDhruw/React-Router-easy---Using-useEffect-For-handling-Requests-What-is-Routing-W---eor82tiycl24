92854857224a297c3704a28f563d3a2b547a92f5
KunalDhruw <108332747+KunalDhruw@users.noreply.github.com>
2023-01-27 20:10:13 +0530

    Update NotFound.js

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,7 +1,5 @@
-import React from 'react';
+import React from "react";
 
 export const NotFound = () => {
-    return (
-       
-    )
-}
+  return <div id="not-found-page">Not found page</div>;
+};
